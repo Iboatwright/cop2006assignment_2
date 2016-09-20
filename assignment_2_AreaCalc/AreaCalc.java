f6c253bd27fec3cd36733763d4a574e1ba3456be
iboatwright <ijboatwright@gmail.com>
2016-09-20 10:18:05 -0400

    branched to using switch method and added some cases.  need to format output.

--- assignment_2_AreaCalc/AreaCalc.java
+++ assignment_2_AreaCalc/AreaCalc.java
@@ -15,37 +15,34 @@ public class AreaCalc {
 	 * @param args
 	 */
 	
-	// tests if user input is equal to 1 or 2.
-	public static boolean validChoice(int choice){
-		boolean isValid = true; // default return value is true
-		if ((choice != 2) && (choice != 1)){ // if user input is not 1 or 2
-			isValid = false; // set return value to false
-		}
-		return isValid; // returns the boolean true or false
-	}
-	
 	// multiplies the two inputs then divides them by 2 
 	public static double calcTriangleArea(int b, int h){
 		return ((b * h)/2.0); // returns the calculated area
 	}
 	
 	// raises the input to the second power then multiplies by pi
-	public static double calcCircleArea(int r){
+	public static double calcCircleArea(double r){
 		return ((double)(Math.PI * Math.pow(r, 2))); //returns calculated area
 	}
 	
 	public static void main(String[] args) {
-		// TODO Need to create git repo and branch this to use the switch statement and 
-		//		not the validChoice method.
+		// TODO format output values to match assignment requirements
 		int choice;
+		double r;
 		Scanner in = new Scanner(System.in);
 		System.out.print("Please enter either 1 or 2: ");
 		choice = in.nextInt();
-		if (!validChoice(choice)){
-			
-		}
 		switch (choice){
 			case 1: 
+				System.out.print("Please enter a non-negative number for radius: ");
+				r = in.nextDouble();
+				if (r < 0){
+					System.out.println("Number " + r + " entered is negative. Program exits.");
+				}
+				else {
+					System.out.println("Area of a circle with radius="+r+": "+ calcCircleArea(r));
+				}
+				
 				break;
 			case 2:
 				break;
