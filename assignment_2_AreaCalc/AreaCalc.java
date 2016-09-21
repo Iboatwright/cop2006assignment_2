2dda3fd2bbc9617a7c47957f24b83e320305db82
iboatwright <ijboatwright@gmail.com>
2016-09-20 23:49:26 -0400

    mostly finished... hate on java output formatting... gods bless regex...

--- assignment_2_AreaCalc/AreaCalc.java
+++ assignment_2_AreaCalc/AreaCalc.java
@@ -6,7 +6,7 @@ public class AreaCalc {
 	
 	// multiplies the two inputs then divides them by 2 
 	public static double calcTriangleArea(int b, int h){
-		return ((b * h)/2.0); // returns the calculated area
+		return ((double)((b * h)/2.0)); // returns the calculated area
 	}
 	
 	// raises the input to the second power then multiplies by pi
@@ -15,32 +15,68 @@ public class AreaCalc {
 	}
 	
 	public static void main(String[] args) {
-		// TODO format output values to match assignment requirements
+		// TODO format output values to truncate trailing zeros
 		int choice; //user menu selection variable
 		double r; 	//radius
-		Scanner in = new Scanner(System.in); //declare and initialize Scanner object
+		int b;	 	//base
+		int h;		//height
+	    String a;	//calculated area
+		Scanner in = new Scanner(System.in); //initialize Scanner object
 		
 		//User selection menu
 		System.out.print("Please enter either 1 or 2: ");
 		choice = in.nextInt();
 		switch (choice){
 			case 1: 
-				
-				break;
-			case 2:
-				System.out.print("Please enter a non-negative number for radius: ");
-				r = in.nextDouble();
-				if (r < 0){
-					System.out.println("Number " + r + " entered is negative. Program exits.");
+				System.out.print("Please enter two non-negative integers for "
+						+ "base and height: ");
+				b = in.nextInt();
+				h = in.nextInt();
+				if ((b < 0) || (h < 0)){
+					int err;//
+					err = (b<0)?b:h; //assign the negative number to err var
+					System.out.println("Number " + err + " entered is "
+							+ "negative. Program exits.");
+					System.exit(0);
 				}
 				else {
-					System.out.println("Area of a circle with radius="+r+": "+ calcCircleArea(r));
+					a = "" + calcTriangleArea(b,h); //get triangle area
+					// format area for output
+					a = a.indexOf(".") < 0 ? a : 
+						a.replaceAll("([0-9]{1,3})*$", "$1").replaceAll("0*$", "").replaceAll("\\.$", "");
+					
+					a = a.indexOf(".") < 0 ? a + ".0":a;
+					System.out.print("Area of a triangle with base=" + b +
+							", height=" + h +": "+ a);
+							
+//					System.out.format("%.3f%n", calcTriangleArea(b,h));
+				}
+				break;
+			case 2:
+				System.out.print("Please enter a non-negative number for "
+						+ "radius: ");
+				r = in.nextDouble();
+				if (r < 0){
+					System.out.println("Number " + r + " entered is negative. "
+							+ "Program exits.");
+					System.exit(0);
+				}
+				else {
+					a = "" + calcCircleArea(r); //get circle area
+//					a = "" + calcCircleArea(r);
+					// format area for output
+					a = a.indexOf(".") < 0 ? a : 
+						a.replaceAll("([0-9]{1,3})*$", "$1").replaceAll("0*$", "").replaceAll("\\.$", "");
+					a = a.indexOf(".") < 0 ? a + ".0":a;
+					System.out.print("Area of a circle with radius="+r+": " 
+							+ a);
+//					System.out.format("%.3f%n", calcCircleArea(r));
 				}
 				
 				break;
 			default:
-				System.out.println("Please rerun the program entering either " +
-					"1 or 2.");
+				System.out.println("Please rerun the program entering either "
+						+ "1 or 2.");
 				System.exit(0);
 		}
 	}
